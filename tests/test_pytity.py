78fd86d8d5f32dc0ceae60b1fd7eda3e1f997465
Marien Fressinaud <dev@marienfressinaud.fr>
2015-01-24 21:40:45 +0100

    Add some tests for new features

--- tests/test_pytity.py
+++ tests/test_pytity.py
@@ -6,6 +6,7 @@ from pytity.manager import Manager
 from pytity.entity import Entity
 from pytity.component import Component
 from pytity.processor import EntityProcessor, Processor
+from pytity.event import Event
 
 
 def test_entity_overall_success():
@@ -26,6 +27,19 @@ def test_entity_without_manager_fail():
 
     with pytest.raises(AttributeError):
         entity.get_component(Component)
+
+    with pytest.raises(AttributeError):
+        entity.del_component(Component)
+
+
+def test_entity_del_component_success():
+    manager = Manager()
+    entity = manager.create_entity()
+    component = Component(42)
+    entity.add_component(component)
+
+    entity.del_component(Component)
+    assert entity.get_component(Component) is None
 
 
 def test_manager_kill_entity_not_existing_fail():
@@ -72,6 +86,14 @@ def test_manager_entities_by_types_two_mixed_success():
     assert entity not in entities
 
 
+def test_manager_del_component_not_set_dont_fail():
+    manager = Manager()
+    entity = manager.create_entity()
+
+    entity.del_component(Component)
+    assert True
+
+
 def test_manager_add_processor_success():
     manager = Manager()
     processor = Processor()
@@ -98,6 +120,14 @@ def test_manager_update_processor_inherited_success():
     assert entity.get_component(Component).value == 'egg'
 
 
+def test_manager_add_event_success():
+    manager = Manager()
+    event = Event()
+
+    manager.add_event(event)
+    assert event is manager.event_store[Event]
+
+
 def test_processor_update_fail():
     processor = Processor()
 
@@ -105,7 +135,16 @@ def test_processor_update_fail():
         processor.update(0.1)
 
 
-def test_entity_processor_update_fail():
+def test_processor_publish_not_registered_event_fail():
+    manager = Manager()
+    processor = Processor()
+    processor.register_to(manager)
+
+    with pytest.raises(ValueError):
+        processor.publish(Event)
+
+
+def test_entity_processor_update_entity_fail():
     processor = EntityProcessor()
     entity = Entity(42)
 
@@ -151,3 +190,30 @@ def test_entity_processor_update_with_not_corresponding_needed_set_fail():
     manager.update(0.1)
 
     assert entity.get_component(Component).value == 'spam'
+
+
+def test_event_call_success():
+    class SpamEggEvent(Event):
+        def call(self, entity, value):
+            component = entity.get_component(Component)
+            component.value = value
+
+    class SpamEggProcessor(EntityProcessor):
+        def update_entity(self, delta, entity):
+            self.publish(SpamEggEvent, entity, 'egg')
+
+    manager = Manager()
+    entity = manager.create_entity()
+    entity.add_component(Component('spam'))
+    SpamEggProcessor().register_to(manager)
+    SpamEggEvent().register_to(manager)
+    manager.update(0.1)
+
+    assert entity.get_component(Component).value == 'egg'
+
+
+def test_event_call_fail():
+    event = Event()
+
+    with pytest.raises(NotImplementedError):
+        event.call()
